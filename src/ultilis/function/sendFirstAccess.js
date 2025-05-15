b95499ad67c6e8465a58ce118b48a66427d4f0c3
Erick Karl <erickkarl5@gmail.com>
2025-05-15 20:50:56 -0300

    fix: removing useless log

--- src/ultilis/function/sendFirstAccess.js
+++ src/ultilis/function/sendFirstAccess.js
@@ -1,28 +1,25 @@
-const sendGrid = require('../../config/sendGrid');
-const { credentialsAccessHTML } = require('../htmlEmails/credentialsAccess');
+const sendGrid = require("../../config/sendGrid");
+const { credentialsAccessHTML } = require("../htmlEmails/credentialsAccess");
 
 async function sendFirstAccess(user) {
-    try {
-        const htmlExcel = await credentialsAccessHTML(user);
-        console.log('buffer: ', buffer)
+  try {
+    const htmlExcel = await credentialsAccessHTML(user);
 
-        const message = {
-            from: "schmi@dottie.pro",
-            to: user.email,
-            subject: `Dottie - Credenciais de Acesso`,
-            html: htmlExcel,
-        };
+    const message = {
+      from: "schmi@dottie.pro",
+      to: user.email,
+      subject: `Dottie - Credenciais de Acesso`,
+      html: htmlExcel,
+    };
 
-        const resultadoEmail = await sendGrid.configEmailSendGrid(message);
-        return resultadoEmail;
-    } catch (error) {
-        console.log(error)
-        return error;
-    }
+    const resultadoEmail = await sendGrid.configEmailSendGrid(message);
+    return resultadoEmail;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 
-
-
 module.exports = {
-    sendFirstAccess
+  sendFirstAccess,
 };
